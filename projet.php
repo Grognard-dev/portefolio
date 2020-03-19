14ac472496da02c62a14c4b5f0fcb932f3d5660f
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-03-19 16:05:04 +0100

    projet cnrs

--- projet.php
+++ projet.php
@@ -24,4 +24,8 @@
         <a href="simplon-charleville/index.php"><img src="image/simplon.png" alt=""></a>
         <span class="projet-nom">Projet simplon charleville </span>
     </div>
+    <div class="simplon-tech" data-aos="fade-in" data-aos-duration="2000">
+        <a href="projetcnrs/card-actu.php"><img src="image/simplon.png" alt=""></a>
+        <span class="projet-nom">Projet simplon Cnrs </span>
+    </div>
 </div>
